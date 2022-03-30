81d9de32a98a760c5f4d4859d210ba807252db0c
Federico Andrucci <federico.andrucci@gmail.com>
2022-03-30 16:53:15 +0200

    sprint 3

---
+++ it.unibo.radarSystem22/src/it/unibo/radarSystem22/sprint3/interpreters/IApplInterpreter.java
@@ -0,0 +1,6 @@
+package it.unibo.radarSystem22.sprint3.interpreters;
+
+public interface IApplInterpreter {
+
+    public String elaborate( String message );
+}
---
+++ it.unibo.radarSystem22/src/it/unibo/radarSystem22/sprint3/interpreters/LedApplInterpreter.java
@@ -0,0 +1,21 @@
+package it.unibo.radarSystem22.sprint3.interpreters;
+
+import it.unibo.radarSystem22.domain.interfaces.ILed;
+import it.unibo.radarSystem22.domain.utils.ColorsOut;
+
+public class LedApplInterpreter implements IApplInterpreter {
+
+    private ILed led;
+
+    public LedApplInterpreter(  ILed led) {
+        this.led = led;
+    }
+
+    public String elaborate( String message ) {
+        ColorsOut.out("LedApplInterpreter | elaborate String=" + message  + " led="+led, ColorsOut.GREEN);
+        if( message.equals("getState") ) return ""+led.getState() ;
+        else if( message.equals("on")) led.turnOn();
+        else if( message.equals("off") ) led.turnOff();
+        return message+"_done";
+    }
+}
